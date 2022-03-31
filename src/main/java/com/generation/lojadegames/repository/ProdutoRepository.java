46786e42ee56594a36d77d45166b62cbf7486486
Ana-Gatti <anagatti@live.com>
2022-03-31 17:19:08 -0300

    Back-end loja de games

---
+++ src/main/java/com/generation/lojadegames/repository/ProdutoRepository.java
@@ -0,0 +1,18 @@
+package com.generation.lojadegames.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.lojadegames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+	public List<Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+
+	public List<Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+
+}
